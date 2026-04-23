927f9d4e1e7d820ee4da4fef2c49768055b97d4f
amisamfaiz10 <amisamfaiz10@gmail.com>
2026-04-23 23:37:10 +0530

    Update urls.py

--- agent/urls.py
+++ agent/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import chat_view, chat_page
+from .views import chat_view, chat_page, reset_chat
 
 urlpatterns = [
     path('chat/', chat_view, name='chat_api'),
     path('', chat_page, name='chat_ui'),
+    path("reset/", reset_chat,name='reset_chat'),
 ]
